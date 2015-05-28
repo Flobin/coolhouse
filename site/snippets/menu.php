1f8dc49aa534fe6ce9c3d2db4eb90405b72ae0b3
Robin Berghuijs <robin@robinberghuijs.com>
2015-05-28 17:30:31 +0200

    set up responsive menu and disable google maps scrollwheel

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,15 +1,17 @@
 <nav role="navigation">
 
+  <input type="checkbox" id="toggle" />
+  <label for="toggle" class="toggle"></label>
   <ul class="menu">
     <?php foreach($pages->visible() as $p): ?>
-    <li>
+    <li class="parent">
       <a class="menu-link" href="#<?php echo $p->title() ?>"><?php echo $p->title()->html() ?></a>
 
     </li>
     <?php endforeach ?>
     <?php foreach($site->languages() as $language): ?>
-    <li <?php e($site->language() == $language, ' id="language-active"') ?> class="">
-      <a href="<?php echo $page->url($language->code()) ?>" class="menuitem">
+    <li <?php e($site->language() == $language, ' id="language-active"') ?> class="parent">
+      <a href="<?php echo $page->url($language->code()) ?>" class="menu-link">
         <?php echo html($language->name()) ?>
       </a>
     </li>
