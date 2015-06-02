641c0e9b82f6aff4053160772083da4e6381a83a
Robin Berghuijs <robin@robinberghuijs.com>
2015-06-02 16:37:11 +0200

    set up mailchimp contact form, overview svg, htaccess, style and content fixes

--- site/snippets/contact.php
+++ site/snippets/contact.php
@@ -8,25 +8,35 @@
   	<?php echo $data->text()->kirbytext() ?>
   </section>
   <section>
-    <form action="<?php echo $page->url()?>" method="post">
+    <!-- Begin MailChimp Signup Form -->
+    <div id="mc_embed_signup">
+      <form action="//cool-house.us11.list-manage.com/subscribe/post?u=b2ec7f81b5575c608e25383a8&amp;id=5fccc97032" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+          <div class="indicates-required"><span class="asterisk">*</span> <?php echo l::get('required') ?></div>
+          <div class="mc-field-group">
+            <input type="email" value="" name="EMAIL" class="required email mc-input" id="mce-EMAIL">
+            <label class="mc-label" for="mce-EMAIL"><span class="mc-label-content"><?php echo l::get('email') ?><span class="asterisk">*</span></span>
+            </label>
+          </div>
+          <div class="mc-field-group">
+            <input type="text" value="" name="FNAME" class="required mc-input" id="mce-FNAME">
+            <label class="mc-label" for="mce-FNAME"><span class="mc-label-content"><?php echo l::get('firstname') ?> <span class="asterisk">*</span></span>
+            </label>
+          </div>
+          <div class="mc-field-group">
+            <input type="text" value="" name="LNAME" class="mc-input" id="mce-LNAME">
+            <label class="mc-label" for="mce-LNAME"><span class="mc-label-content"><?php echo l::get('lastname') ?> </label></span>
+          </div>
+          <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+          </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;"><input type="text" name="b_b2ec7f81b5575c608e25383a8_5fccc97032" tabindex="-1" value=""></div>
+          <div class="clear"><input type="submit" value="<?php echo l::get('submit') ?>" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </div>
+      </form>
+    </div>
 
-      <label<?php e($form->hasError('_from'), ' class="erroneous"')?> for="email">E-Mail</label>
-      <input type="email" name="_from" id="email" value="<?php $form->echoValue('_from') ?>" required/><br>
-
-      <label for="message"><?php echo l::get('message') ?></label>
-      <textarea name="message" id="message"><?php $form->echoValue('message') ?></textarea>
-
-      <label class="uniform__potty" for="website">Please leave this field blank</label>
-      <input type="text" name="website" id="website" class="uniform__potty" /><br>
-
-      <?php if ($form->hasMessage()): ?>
-      <div class="message <?php e($form->successful(), 'success' , 'error')?>">
-        <?php $form->echoMessage() ?>
-      </div>
-      <?php endif; ?>
-
-      <button class="submit" type="submit" name="_submit" value="<?php echo $form->token() ?>"<?php e($form->successful(), ' disabled')?>><?php echo l::get('submit') ?></button>
-
-    </form>
+    <!--End mc_embed_signup-->
   </section>
 </article>
--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -15,12 +15,14 @@
   <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA3b9pBpSLNDZoC6jt-kJXadoOxm-g5Jgo&sensor=false"></script>
   <?php echo js('/assets/build/js/production.js') ?>
   <script>
-    'use strict';
+    //'use strict';
     document.onreadystatechange = function () {
       if (document.readyState == "complete") {
         var root = $('html, body'),
             menu = $('.menu'),
-            article = $('.homepage-article');
+            article = $('.homepage-article'),
+            input = $(".mc-input");
+
         $('.menu-link').click(function() {
           var href = $.attr(this, 'href');
           root.stop(true,true).animate({
@@ -32,12 +34,8 @@
         });
 
         var header = document.getElementById('site-header')
-        // make '.nav' stick to the top
         sticky(header);
 
-        //var nav = document.getElementById('nav');
-        //sticky(nav);
-        //window.fitText( document.getElementById("responsive-headline") );
         var slabTextHeadlines = function() {
           $(".slab-headline").slabText({
             // Don't slabtext the headers if the viewport is under 380px
@@ -53,37 +51,20 @@
           $('.active').removeClass('active');
           menu.find('a[href="#' + this.id + '"]').addClass('active');
         });
-
         article.on('scrollSpy:exit', function() {
           console.log('exit:', $(this).attr('id'));
         });
-
         article.scrollSpy();
 
         var initMap = function() {
-          // Basic options for a simple Google Map
-          // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
           var mapOptions = {
-              // How zoomed in you want the map to start at (always required)
               zoom: 17,
-
-              // The latitude and longitude to center the map (always required)
-              center: new google.maps.LatLng(52.096875, 4.2679705), // New York
-
+              center: new google.maps.LatLng(52.096875, 4.2679705),
               scrollwheel: false,
-              // How you would like to style the map. 
-              // This is where you would paste any style found on Snazzy Maps.
               styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"visibility":"simplified"},{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#00a1ff"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.place_of_worship","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"transit.line","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station.rail","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#61c5ff"},{"visibility":"simplified"}]}]
           };
-
-          // Get the HTML DOM element that will contain your map 
-          // We are using a div with id="map" seen below in the <body>
           var mapElement = document.getElementById('locatie-map');
-
-          // Create the Google Map using our element and options defined above
           var map = new google.maps.Map(mapElement, mapOptions);
-
-
           // Let's also add a marker while we're at it
           var marker = new google.maps.Marker({
               position: new google.maps.LatLng(52.096310, 4.267894),
@@ -92,6 +73,13 @@
           });
         }
         initMap();
+
+        input.focus(function() {
+          $(this).addClass("input-focus")
+        });
+        input.blur(function() {
+          $(this).removeClass("input-focus")
+        });
 
       };
     }
