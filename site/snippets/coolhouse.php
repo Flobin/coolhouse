bdc50003115817a675e5236c1c04de33629e00dd
Robin Berghuijs <robin@robinberghuijs.com>
2015-06-03 16:42:24 +0200

    fix order, add horizontal logo, attempt to fix htaccess

--- site/snippets/coolhouse.php
+++ site/snippets/coolhouse.php
@@ -1,4 +1,3 @@
 <article id="Coolhouse" class="homepage-article">
-  <h1 class="slab-headline"><span class="slabtext"><?php echo $data->title()->html() ?></span></h1>
   <?php echo $data->text()->kirbytext() ?>
 </article>
